d6fe1c339c3038fafe6f5393196468107473a4e8
Nathan <nbanmp@gmail.com>
2018-02-02 10:57:59 -0500

    removed useless migration

--- migrations/2_ctf_coin.js
+++
@@ -1,7 +0,0 @@
-var CTFCoin = artifacts.require("CTFCoin");
-var MyContract = artifacts.require("MyContract");
-
-module.exports = function(deployer) {
-  deployer.deploy(CTFCoin, "CTF Coin", "CTF", 8);
-  deployer.deploy(MyContract);
-};
